cb97ba97305b531f18565ec0970c353c6226dcb3
Romina Loor <116986959+rominaisa1981@users.noreply.github.com>
2026-05-10 20:41:35 -0500

    Update API URL for production environment

--- presupuesto-app/src/environments/environment.ts
+++ presupuesto-app/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   // Usa el puerto HTTP para evitar problemas de certificado en dev
   // Si prefieres HTTPS, cambia a 'https://localhost:7100/api'
-  apiUrl: 'https://localhost:7100/api'
+  apiUrl: 'https://api.finanzasecuador.com/api'
 };
